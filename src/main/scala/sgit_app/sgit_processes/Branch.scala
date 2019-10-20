fd5e6c126bf4187aecee9edbec0740eabdb5174e
Antoine <antoine.sanchez@etu.umontpellier.fr>
2019-10-20 11:18:05 +0200

    Add sgit branch and tests

--- src/main/scala/sgit_app/sgit_processes/Branch.scala
+++ src/main/scala/sgit_app/sgit_processes/Branch.scala
@@ -1,14 +1,62 @@
 package sgit_app.sgit_processes
 
+import better.files._
+import java.nio.file.{Files, Paths}
 import sgit_app.io._
+import sgit_app.sgit_objects._
 
 object Branch {
 
   /**
   * List or create branches
   */
-  def sgit_branch(branch: String = "", option: String = ""): String = {
-    ""
+  def sgit_branch(branchName: String = "", option: String = "", target: String = ""): String = {
+    if(!Files.exists(Paths.get(target + ".sgit"))
+    || !Files.exists(Paths.get(target + ".sgit/objects"))
+    || !Files.exists(Paths.get(target + ".sgit/HEAD"))) return "fatal: not a sgit repository"
+    if((branchName == "" && option == "")
+    || (branchName != "" && option != "")
+    || (option != "" && option != "av")) return "sgit branch\nusage:\nsgit branch <branch name>\nsgit branch -av"
+    val branch = (target + ".sgit/HEAD")
+      .toFile
+      .contentAsString
+      .split("/")
+      .last
+    val ref = target + ".sgit/refs/heads/" + branch
+    if(!Files.exists(Paths.get(ref))) return "fatal: Not a valid object name: '" + branch + "'."
+    if (branchName != "") {
+      val branchFile = target + ".sgit/refs/heads/" + branchName
+      if(Files.exists(Paths.get(branchFile))) return "fatal: A branch named '" + branchName + "' already exists"
+      val parentHash = ref
+        .toFile
+        .contentAsString
+        .split(" ")
+        .last
+      val commit = "0000000000000000000000000000000000000000 " + parentHash + "\n"
+      Tools.createDirOrFile(false, branchFile)
+      Tools.writeFile(branchFile, commit)
+      return ""
+    } else {
+      val branchList = (target + ".sgit/refs/heads")
+        .toFile
+        .children
+        .toList
+        .sortWith(_.name < _.name)
+        .map(child => {
+          if (child.name == branch) "* " + child.name
+          else "  " + child.name
+        })
+        .foldLeft("branches:\n")((res, line) => res + line + "\n")
+      val tagList = (target + ".sgit/refs/tags")
+        .toFile
+        .children
+        .toList
+        .sortWith(_.name < _.name)
+        .map(child => "  " + child.name)
+        .foldLeft("tags:\n")((res, line) => res + line + "\n")
+      return branchList + tagList
+    }
+
   }
 
 }
---
+++ src/test/scala/sgit_app_tests/BranchTest.scala
@@ -0,0 +1,71 @@
+package sgit_app_tests
+
+import org.scalatest._  
+import better.files._
+import java.nio.file.{Files, Paths}
+import sgit_app.io.Tools
+import sgit_app.sgit_processes.{Tag => STag, _}
+
+class BranchTest extends FunSpec with BeforeAndAfter {
+
+  before {
+    Tools.createDirOrFile(true, "test") 
+    Init.sgit_init("test/")
+    Tools.createDirOrFile(false,"test/a.txt")
+    Tools.createDirOrFile(false,"test/a.c")
+    Tools.writeFile("test/a.txt", "This is a test in a text file")
+    Tools.writeFile("test/a.c", "This is a test in a c file")
+  }
+
+  after {
+    Tools.delete("test")
+  }
+
+  describe("sgit branch") {
+
+    describe("before a commit") {
+
+      it("Should throw an error") {
+        assert(Branch.sgit_branch("branch", "", "test/") == "fatal: Not a valid object name: 'master'.")
+      }
+    
+    }
+
+    describe("after a commit") {
+
+      it("Should create the branch succesfully if not already used") {
+        Add.sgit_add(Seq("."), "test/")
+        Commit.sgit_commit("test/")
+        Branch.sgit_branch("branch", "", "test/")
+        val lastCommitHash = "test/.sgit/refs/heads/master"
+          .toFile
+          .contentAsString
+          .split(" ")
+          .last
+        assert(Files.exists(Paths.get("test/.sgit/refs/heads/branch")))
+        assert("test/.sgit/refs/heads/branch".toFile.contentAsString.contains(lastCommitHash))
+      }
+
+      it("Shouldn't allow a user to create the same tag twice") {
+        Add.sgit_add(Seq("."), "test/")
+        Commit.sgit_commit("test/")
+        assert(Branch.sgit_branch("branch", "", "test/") == "")
+        assert(Branch.sgit_branch("branch", "", "test/") == "fatal: A branch named 'branch' already exists")
+      }
+
+      it("Should list branches and tags if the option is av") {
+        Add.sgit_add(Seq("."), "test/")
+        Commit.sgit_commit("test/")
+        Branch.sgit_branch("branch", "", "test/")
+        Branch.sgit_branch("zranch", "", "test/")
+        STag.sgit_tag("tag", "test/")
+        STag.sgit_tag("bag", "test/")
+        assert(Branch.sgit_branch("", "av", "test/").contains("branches:\n  branch\n* master\n  zranch"))
+        assert(Branch.sgit_branch("", "av", "test/").contains("tags:\n  bag\n  tag"))
+      }
+
+    }
+  
+  }
+      
+}
